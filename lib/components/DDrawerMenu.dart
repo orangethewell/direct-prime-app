4f347d3b995f6b52cd664b3165a07eb92ce379f8
Wellington Júnior <67232334+orangethewell@users.noreply.github.com>
2024-12-11 11:55:00 -0300

    feat: :egg: Update names

--- lib/components/DDrawerMenu.dart
+++ lib/components/DDrawerMenu.dart
@@ -21,8 +21,8 @@ class DDrawerMenu extends StatelessWidget {
                   backgroundImage: NetworkImage("https://media.istockphoto.com/id/910563528/pt/foto/handsome-man.jpg?s=1024x1024&w=is&k=20&c=TESzB4KaNtKBmi8BwfqOUppgUlwr2pM28YCQZqGbUH0=")
                 ),
                 SizedBox(height: 12),
-                Text('Rivaldo Jesus', style: TextStyle(fontSize: 28, color: Theme.of(context).colorScheme.onPrimary)),
-                Text('Espírito Santo', style: TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.onPrimary)),
+                Text('José da Silva', style: TextStyle(fontSize: 28, color: Theme.of(context).colorScheme.onPrimary)),
+                Text('Minas Gerais', style: TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.onPrimary)),
                 SizedBox(height: 12),
               ],
             ),
